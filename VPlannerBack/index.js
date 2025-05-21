deabbc659dad6952ad307b7cdaa47d71bd4cd98c
Foued-pro <foued.attar@lyceeastier.com>
2025-05-21 09:25:00 +0200

    changement du CORS pour test les post

--- VPlannerBack/index.js
+++ VPlannerBack/index.js
@@ -9,9 +9,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: ['https://kyotsuvoyage.vercel.app', 'http://localhost:3000']
-}));
+
+app.use(cors());
+
+//app.use(cors({
+//  origin: ['https://kyotsuvoyage.vercel.app', 'http://localhost:3000']
+//}));
 
 app.use(
   helmet.contentSecurityPolicy({
